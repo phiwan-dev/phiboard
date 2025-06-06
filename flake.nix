9fb4e824eb9738dc9488eacab4b76b29afaf130c
phiwan-dev <75113544+phiwan-dev@users.noreply.github.com>
2025-06-07 01:12:38 +0200

    dev: add more tooling to flake

--- flake.nix
+++ flake.nix
@@ -1,8 +1,6 @@
 {
     description = "ergogen dev env";
-
-    #inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";       Uses the cuda driver installed on the system, hence commented out.
-    #inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";    Forcing a specific version will likely result in conflicts!
+    inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
     outputs = { self, nixpkgs }:
     let
@@ -13,9 +11,13 @@
 
             buildInputs = with pkgs; [
               ergogen
+              librecad  # for 2D outlines
+              kicad     # for PCBs
             ];
 
             shellHook = ''
+              export __GLX_VENDOR_LIBRARY_NAME="" # To fix kicad with nvidia drivers
+
               echo loaded ergogen shell
             '';
         };
